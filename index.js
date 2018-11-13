9fc3c955b2e6d26d296f583f3fafaab350efd304
Joni Virtanen <vijoni91@gmail.com>
2018-11-13 21:05:40 +0200

    3.2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const data = '<p> puhelinluettolossa on ' + persons.length + ' henkilön tiedot</p>' +
+                 '<p>' + new Date().toString() + '</p>'
+    res.send(data)
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
