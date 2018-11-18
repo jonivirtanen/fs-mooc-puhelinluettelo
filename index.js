04d3b9c0ec4e1ce8fe02e9b7905b021e1308d926
Joni Virtanen <vijoni91@gmail.com>
2018-11-18 18:39:55 +0200

    3.16

--- index.js
+++ index.js
@@ -58,10 +58,14 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
+    Person
+      .findByIdAndRemove(req.params.id)
+      .then(result => {
+          res.status(204).end()
+      })
+      .catch(error => {
+          res.status(400).send({error: 'no such id'})
+      })
 })
 
 const PORT = process.env.PORT || 3001
