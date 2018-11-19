1f3c3a492eceb88f8aaa9cd91c597c7b8dc8a054
Joni Virtanen <vijoni91@gmail.com>
2018-11-19 09:45:41 +0200

    3.20

--- build/precache-manifest.12e6a905a0e1c4b684f54efc69e13fc4.js
+++ build/precache-manifest.12e6a905a0e1c4b684f54efc69e13fc4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "178ed807b22f125712e1",
-    "url": "/static/js/main.178ed807.chunk.js"
+    "revision": "f4976e3b5950a962e6c9",
+    "url": "/static/js/main.f4976e3b.chunk.js"
   },
   {
     "revision": "81cfa7dd27146b4842c0",
     "url": "/static/js/1.81cfa7dd.chunk.js"
   },
   {
-    "revision": "178ed807b22f125712e1",
+    "revision": "f4976e3b5950a962e6c9",
     "url": "/static/css/main.98fb60bc.chunk.css"
   },
   {
-    "revision": "4926720b3e91f22f78d6b2ce0efd2d29",
+    "revision": "1d79ae9a71a0d1a5872462522db126a9",
     "url": "/index.html"
   }
 ];
